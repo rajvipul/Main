f2f2b0e3934ac1eefd1b4575b670fe1a63c43f02
rajvipul <raj.vipul97@gmail.com>
2020-01-27 11:21:10 +0530

    Initial commit:Prime

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner scnr = new Scanner(System.in);
+        int number = Integer.MAX_VALUE;
+        System.out.println("Enter number to check if prime or not ");
+        while (number != 0) {
+            number = scnr.nextInt();
+            System.out.printf("Is %d a prime? %s  %n", number,
+                    isPrimeNumber(number));
+        }
+    }
+
+
+    public static boolean isPrimeNumber(int number) {
+        if (number == 2 || number == 3) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(number) + 1;
+        for (int i = 3; i < sqrt; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
